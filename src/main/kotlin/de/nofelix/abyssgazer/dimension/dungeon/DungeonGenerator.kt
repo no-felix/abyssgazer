eae04d2a5d61e3432b13891ed54be106f030c7b4
no-felix <69461004+no-felix@users.noreply.github.com>
2025-09-29 14:16:20 +0200

    🏗️ Add initial classes for dimension

---
+++ src/main/kotlin/de/nofelix/abyssgazer/dimension/dungeon/DungeonGenerator.kt
@@ -0,0 +1,8 @@
+package de.nofelix.abyssgazer.dimension.dungeon
+
+/**
+ * TODO: Generate dungeon layouts, rooms, and traversal logic for each dimension level.
+ */
+object DungeonGenerator {
+    // TODO: Implement dungeon graph construction and placement routines.
+}
---
+++ src/main/kotlin/de/nofelix/abyssgazer/dimension/generation/DimensionManager.kt
@@ -0,0 +1,8 @@
+package de.nofelix.abyssgazer.dimension.generation
+
+/**
+ * TODO: Handle procedural generation of the Abyss dimension layers.
+ */
+object DimensionManager {
+    // TODO: Implement dimension registration and runtime management logic.
+}
