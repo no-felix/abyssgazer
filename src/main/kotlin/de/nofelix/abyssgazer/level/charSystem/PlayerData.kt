6d7090a7e5941454148ebbebbaa550cdc348f7af
Denis <denisoz583@gmail.com>
2025-09-29 19:27:13 +0200

    🏗️ Add initial classes for level

---
+++ src/main/kotlin/de/nofelix/abyssgazer/level/charSystem/PlayerData.kt
@@ -0,0 +1,10 @@
+package de.nofelix.abyssgazer.level.charSystem
+
+class PlayerData {
+    /**
+     * This class is used to store and manage the player's data throughout the game.
+     * PlayerData holds all relevant information about the player character, including the Experience, Level, Class etc...
+     * It provides methods to update and retrieve this data as needed.
+     * Integrates with LevelManager to ensure that player data is consistent with the character's progression.
+     */
+}
