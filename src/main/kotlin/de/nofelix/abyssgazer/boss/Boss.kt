f25a416de5ad66dd755f42fb5fda0cfcb27cb4a5
allixon <allixonkb@proton.me>
2025-09-29 20:59:41 +0200

    🏗️ add boss interface

---
+++ src/main/kotlin/de/nofelix/abyssgazer/boss/Boss.kt
@@ -0,0 +1,8 @@
+package de.nofelix.abyssgazer.boss
+
+interface Boss {
+    /**
+     * Defines certain attributes that every boss should have.
+     * Every Boss has a name, a certain amount of HP, (possible color of Boss Bar) etc.
+     */
+}
