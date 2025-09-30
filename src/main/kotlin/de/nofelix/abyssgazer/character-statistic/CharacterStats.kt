d9d2fd80a20dd86b66cec5f9b06b6df88bf34f55
Lide Huang <huang.lide@gmx.de>
2025-09-30 11:32:17 +0200

    ✨ implement Stats data class and update CharacterStats structure

--- src/main/kotlin/de/nofelix/abyssgazer/character-statistic/CharacterStats.kt
+++ src/main/kotlin/de/nofelix/abyssgazer/character-statistic/CharacterStats.kt
@@ -2,9 +2,23 @@ package de.nofelix.abyssgazer.`character-statistic`
 
 enum class StatType { HP, STRENGTH, DEXTERITY, INTELLIGENCE, FAITH }
 
-//TODO: data class zum halten der stats erstellen
+data class Stats(
+    val stats: MutableMap<StatType, Int> = mutableMapOf(
+        StatType.HP to 10,
+        StatType.STRENGTH to 10,
+        StatType.DEXTERITY to 10,
+        StatType.INTELLIGENCE to 10,
+        StatType.FAITH to 10,
+    )
+)
 
-class CharacterStats {
-
+class CharacterStats(
+    val name : String,
+    var xp: Int,
+    var level: Int,
+    var levelUpPoints: Int,
+    val stats: Stats = Stats()
+){
+    //TODO: impl character stat logic here
 }
 
